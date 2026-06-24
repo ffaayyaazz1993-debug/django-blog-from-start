5fb4d1c53441b5d517986ca2ab9b3b2caa45042d
ffaayyaazz1993-debug <ffaayyaazz1993@gmail.com>
2026-06-24 12:10:28 +0530

    Add SECRET_KEY to build script

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
+export SECRET_KEY="x4sdfWGT7eZm5F3weeTpDF_8mcbDu8LNxjlL8ND18TXh3PkX72g3yi-rPkoRsh9BBBw"
 python manage.py collectstatic --no-input
 python manage.py migrate
