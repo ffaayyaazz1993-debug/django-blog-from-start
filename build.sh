3e3262c699487d70ce0420dd18467004d19ad6a4
ffaayyaazz1993-debug <ffaayyaazz1993@gmail.com>
2026-06-24 12:34:33 +0530

    Add superuser creation to build script

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 export SECRET_KEY="x4sdfWGT7eZm5F3weeTpDF_8mcbDu8LNxjlL8ND18TXh3PkX72g3yi-rPkoRsh9BBBw"
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py create_superuser --username admin --email admin@example.com --password admin123456
